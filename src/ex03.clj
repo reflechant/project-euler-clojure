effaf8d4261fccf1026b48f7d164976200a62264
reflechant <gr.evocatus@gmail.com>
2018-01-06 22:23:15 +0300

    ...

--- src/ex03.clj
+++ src/ex03.clj
@@ -1,4 +1,11 @@
 (ns ex03)
 
+(defn prime-numbers
+    ([] (prime-numbers ([2] 3)))
+    ([vec n]
+     (if (some (comp zero? (partial rem n)) vec) ; if n is divisible by any x in vec
+         (lazy-seq (prime-numbers vec (inc n))) ; not a prime number, proceed to n+1
+         (lazy-seq (prime-numbers (conj vec n) (inc n)))))) ; else - found new prime number, add to vec, proceed to n+1
+
 (defn -main []
-    (println "not ready yet"))
+    (println (take 10 (prime-numbers))))
