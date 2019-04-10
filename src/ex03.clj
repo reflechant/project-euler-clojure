3cef667884832f579b26b784e57e793d6e7ad926
R.G <reflechant@users.noreply.github.com>
2019-04-10 12:39:41 +0300

    ex03 almost working

--- src/ex03.clj
+++ src/ex03.clj
@@ -1,16 +1,15 @@
 (ns ex03)
 
 (defn next-prime
-    ([coll] (next-prime coll (inc(peek coll))))
-    ([coll n] (if (some (comp zero? (partial rem n)) coll)
-                (next-prime coll (inc n))
-                (conj coll n))))
+  ([xs] (next-prime xs (inc (last xs))))
+  ([xs x]
+   (if (some #(zero? (rem x %)) xs)
+     (next-prime xs (inc x))
+     x)))
 
-(defn prime-numbers []
-    (iterate next-prime [2]))
+(defn primes
+  ([] (primes [2]))
+  ([xs]
+   (lazy-seq (conj xs (next-prime xs)))))
 
-(defn -main []
-    ; (println (take 5 (iterate fib '(2 1)))))
-    (println (take 10 (prime-numbers))))
-    ; (println (take 5 (iterate next-prime-coll '(2)))))
-    ; (println (next-prime (lazy-seq [2 3]) 4)))
+(println (take 10 (primes)))
