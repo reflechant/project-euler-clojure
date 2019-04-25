59e57f9e98d412b7fd01ecc7570cb2e5db129595
R.G <reflechant@users.noreply.github.com>
2019-04-25 20:19:12 +0300

    calculating actual factors based from prime numbers - in progress

--- src/ex03.clj
+++ src/ex03.clj
@@ -1,14 +1,25 @@
 (ns ex03)
 
+(def N 600851475143)
+
 (defn next-prime
-  ([xs] (next-prime xs (inc (first xs))))
+  ([xs]
+   (next-prime xs (inc (first xs))))
   ([xs x]
    (if (some #(zero? (rem x %)) xs)
      (recur xs (inc x))
      x)))
 
 (defn primes []
-   (map first (iterate (fn [xs] (cons (next-prime xs) xs)) '(2))))
+   (map first
+        (iterate
+         (fn [xs] (cons (next-prime xs) xs))
+         '(2))))
 
-(defn main []
-  (println (take 100 (primes))))
+(defn factors [x]
+         (filter
+          #(zero? (rem x %))
+          (take-while (< x) (primes))))
+
+(defn -main []
+  (println (factors 10)))
