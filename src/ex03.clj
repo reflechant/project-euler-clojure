dfe24020baad9239990483714b6bcd6531741bb7
Roman Gerasimoff <reflechant@users.noreply.github.com>
2019-04-28 00:21:06 +0300

    reformat

--- src/ex03.clj
+++ src/ex03.clj
@@ -3,23 +3,20 @@
 (def N 600851475143)
 
 (defn next-prime
-  ([xs]
-   (next-prime xs (inc (first xs))))
-  ([xs x]
-   (if (some #(zero? (rem x %)) xs)
-     (recur xs (inc x))
-     x)))
+  ([xs] (next-prime xs (inc (first xs))))
+  ([xs x] (if (some #(zero? (rem x %)) xs)
+            (recur xs (inc x))
+            x)))
 
 (defn primes []
-   (map first
-        (iterate
-         (fn [xs] (cons (next-prime xs) xs))
-         '(2))))
+  (map first (iterate
+               (fn [xs] (cons (next-prime xs) xs))
+               '(2))))
 
 (defn factors [x]
-         (filter
-          #(zero? (rem x %))
-          (take-while (< x) (primes))))
+  (filter
+    #(zero? (rem x %))
+    (take-while (< x) (primes))))
 
 (defn -main []
   (println (factors 10)))
